5a1d4e915ab7b802805823efa7535e9fb9232b0c
Jason Penney <jpenney@jczorkmid.net>
2016-01-07 19:34:14 -0500

    applied 0005-Fix-absence-of-_byteswap_ulong-on-Linux.patch

--- mp3framehuffman-c.c
+++ mp3framehuffman-c.c
@@ -332,7 +332,7 @@ static unsigned int get_bits_slow(seq_string_t *s, int num_bits) {
 static unsigned int get_bits_wordwise(seq_string_t *s, int num_bits) {
 	uint32 raw;
 
-#if !defined __CYGWIN__
+#if defined(__WIN32__) && !defined __CYGWIN__
 	uint32 *int_ptr;
 	int_ptr = (uint32 *)s->byte_ptr;
 	raw = _byteswap_ulong(*int_ptr) << s->bit_index;
